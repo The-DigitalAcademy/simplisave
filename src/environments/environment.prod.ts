bda7b704de7b10ca258d2e8bebb92d0d709a23a3
mpendulo-dev <mpendulo.dev@gmail.com>
2023-07-14 10:12:50 +0200

    Setup: eslint and prettier for code formatting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  REG_URL: 'https://example.com/api/users/register',
-  LOGIN_URL: 'https://example.com/api/users/login',
+    production: true,
+    REG_URL: 'https://example.com/api/users/register',
+    LOGIN_URL: 'https://example.com/api/users/login',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  REG_URL: 'http://localhost:8080/api/users/register',
-  LOGIN_URL: 'http://localhost:8080/api/users/login',
+    production: true,
+    REG_URL: 'http://localhost:8080/api/users/register',
+    LOGIN_URL: 'http://localhost:8080/api/users/login',
 };
