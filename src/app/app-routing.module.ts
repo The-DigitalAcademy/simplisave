69eba44349d17944bd98d856517ba0bc744d532b
Lvkoena github <lebodigitalacademy>
2023-07-17 14:52:30 +0200

    updated account route and account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './pages/accounts/accounts.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'accounts', component: AccountsComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
