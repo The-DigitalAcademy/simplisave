9b3ea30bc060f3a9fd23f646046022f11dfd2d92
Mukosi <mukosibudeli@gmail.com>
2023-07-18 16:24:23 +0200

    updated side bar and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,10 @@ import { LearnVsInvestComponent } from './pages/learn/learn-vs-invest/learn-vs-i
 import { LearnBestSuitedComponent } from './pages/learn/learn-best-suited/learn-best-suited.component';
 import { LearnOfferComponent } from './pages/learn/learn-offer/learn-offer.component';
 import { TopPartComponent } from './pages/dashboard/top-part/top-part.component';
-
+import { ChecklistComponent } from './pages/dashboard/checklist/checklist.component';
 
 @NgModule({
-    declarations: [AppComponent, LandingComponent, LearnComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent, AccountsComponent, DashboardComponent, NavbarComponent, SidebarComponent, FooterComponent, LearnBannerComponent, LearnWhySAveComponent, LearnVsNoSavingsComponent, LearnVsInvestComponent, LearnBestSuitedComponent, LearnOfferComponent,TopPartComponent],
+    declarations: [AppComponent, LandingComponent, LearnComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent, AccountsComponent, DashboardComponent, NavbarComponent, SidebarComponent, FooterComponent, LearnBannerComponent, LearnWhySAveComponent, LearnVsNoSavingsComponent, LearnVsInvestComponent, LearnBestSuitedComponent, LearnOfferComponent,TopPartComponent,ChecklistComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
