e5a331f1013653ed44799f31c56ab4a8c378cea1
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-07-21 08:17:59 +0200

    worked on the routing

--- src/app/pages/landing/offers/offers.component.ts
+++ src/app/pages/landing/offers/offers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./offers.component.css']
 })
 export class OffersComponent {
+
+  constructor(private router: Router) {}
+
+navigateToRegisterComponent(): void {
+  this.router.navigateByUrl('/register');
+}
+
+navigateToLearnComponent(): void {
+  this.router.navigateByUrl('/learn');
+}
 
 }
