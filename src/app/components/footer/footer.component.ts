7c16171127320e521f58aaff51431b50f686ca96
Lvkoena github <lebodigitalacademy>
2023-07-23 21:42:16 +0200

    updated footer scroll button

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+/* eslint-disable prettier/prettier */
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent{
+    showScrollButton = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.checkScrollPosition();
+  }
+
+  checkScrollPosition() {
+    this.showScrollButton = window.scrollY > 500;
+  }
+
+  scrollToTop() {
+    // Scroll to the top of the page
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
