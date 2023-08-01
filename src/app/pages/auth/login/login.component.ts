8627b40d28113025b0821475b018ff7ac5f251a5
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-08-01 20:41:27 +0200

    mdified login component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import { Router} from '@angular/router'
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(): void {
+    this.http.get<any>('assets/users.json').subscribe(
+      (data) => {
+        const users = data.posts;
+        const user = users.find(
+          (u: any) =>
+            u.emailaddress === this.loginForm.value.email &&
+            u.password === this.loginForm.value.password
+        );
+        if (user) {
+          alert('Login success!!!');
+          this.loginForm.reset();
+          this.router.navigate(['dashboard']);
+        } else {
+          alert('User not found!!!');
+        }
+      },
+      (error) => {
+        alert('Something went wrong!!!');
+      }
+    );
+  }
 }
