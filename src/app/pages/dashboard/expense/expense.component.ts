ff2fda81b2c7240222a368de94caf8d8b476c931
Mohammed Badat <moebadat16@gmail.com>
2023-08-01 16:21:02 +0200

    We are now able to calc the total spent for each month

--- src/app/pages/dashboard/expense/expense.component.ts
+++ src/app/pages/dashboard/expense/expense.component.ts
@@ -13,52 +13,95 @@ export class ExpenseComponent {
   constructor(private route: ActivatedRoute,private service:DashboardService, private router: Router) {}
 
   chart!: Chart; // Add the "!" symbol to indicate it will be initialized later
-  items1: any = [];
-  data: any;
-  filteredData: any[] = []; // Initialize filteredData as an empty array
-  sumMoneyOut: any;
-  
-  ngOnInit() {
-    this.createChart();
-    this.getDataFromApi();
-  }
-  
-  getDataFromApi() {
-    this.service.getTransactions()
-      .subscribe(res => {
-        this.items1 = res;
-        console.log(this.items1);
-        this.filterData();
-      });
-  }
-  
-  filterData() {
-    // Step 1: Parse the date strings in the JSON data to JavaScript Date objects
-    const transactions = this.items1.map((record: any) => ({
-      ...record,
-      Transaction_Date: new Date(record.Transaction_Date)
-    }));
-  
-    // Step 2: Get the current month and year
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth(); // Get the current month (0 to 11)
-  
-    // Step 3: Filter records where Money_Out is greater than 0 and Transaction_Date is within the current month
-    this.filteredData = transactions.filter((record: any) => {
-      const isMoneyOutPositive = record.Money_Out > 0;
-      const transactionDate = record.Transaction_Date;
-      const isWithinCurrentMonth = transactionDate.getMonth() === currentMonth;
-  
-      return isMoneyOutPositive && isWithinCurrentMonth;
+items1: any = [];
+data: any;
+filteredData: any[] = []; // Initialize filteredData as an empty array
+sumMoneyOut: any;
+sumMoneyOutCurrentMonth: any;
+sumMoneyOutPrev1Month: any;
+sumMoneyOutPrev2Month: any;
+sumMoneyOutPrev3Month: any;
+
+ngOnInit() {
+  this.createChart();
+  this.getDataFromApi();
+}
+
+getDataFromApi() {
+  this.service.getTransactions()
+    .subscribe(res => {
+      this.items1 = res;
+      console.log(this.items1);
+      this.filterData();
     });
+}
+
+filterData() {
+  // Step 1: Parse the date strings in the JSON data to JavaScript Date objects
+  const transactions = this.items1.map((record: any) => ({
+    ...record,
+    Transaction_Date: new Date(record.Transaction_Date)
+  }));
+
+  // Step 2: Get the current month and year
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth(); // Get the current month (0 to 11)
+
+  // Step 3: Filter records where Money_Out is greater than 0 and Transaction_Date is within the current month
+  this.filteredData = transactions.filter((record: any) => {
+    const isMoneyOutPositive = record.Money_Out > 0;
+    const transactionDate = record.Transaction_Date;
+    const isWithinCurrentMonth = transactionDate.getMonth() === currentMonth;
+
+    return isMoneyOutPositive && isWithinCurrentMonth;
+  });
+
+  // Step 4: Calculate the sum of Money_Out for the filtered records of the current month
+  this.sumMoneyOutCurrentMonth = this.filteredData.reduce((sum: number, record: any) => sum + record.Money_Out, 0);
+  console.log("Total Money Spent (Current Month):", this.sumMoneyOutCurrentMonth);
+
+  // Step 5: Filter records for the previous three months
+  const prev1Month = (currentMonth - 1 + 12) % 12;
+  const prev2Month = (currentMonth - 2 + 12) % 12;
+  const prev3Month = (currentMonth - 3 + 12) % 12;
+
+  const filteredPrev1MonthData = transactions.filter((record: any) => {
+    const isMoneyOutPositive = record.Money_Out > 0;
+    const transactionDate = record.Transaction_Date;
+    const isWithinPrev1Month = transactionDate.getMonth() === prev1Month;
+
+    return isMoneyOutPositive && isWithinPrev1Month;
+  });
+
+  const filteredPrev2MonthData = transactions.filter((record: any) => {
+    const isMoneyOutPositive = record.Money_Out > 0;
+    const transactionDate = record.Transaction_Date;
+    const isWithinPrev2Month = transactionDate.getMonth() === prev2Month;
+
+    return isMoneyOutPositive && isWithinPrev2Month;
+  });
+
+  const filteredPrev3MonthData = transactions.filter((record: any) => {
+    const isMoneyOutPositive = record.Money_Out > 0;
+    const transactionDate = record.Transaction_Date;
+    const isWithinPrev3Month = transactionDate.getMonth() === prev3Month;
+
+    return isMoneyOutPositive && isWithinPrev3Month;
+  });
+
+  // Step 6: Calculate the sum of Money_Out for the filtered records of the previous three months
+  this.sumMoneyOutPrev1Month = filteredPrev1MonthData.reduce((sum: number, record: any) => sum + record.Money_Out, 0);
+  this.sumMoneyOutPrev2Month = filteredPrev2MonthData.reduce((sum: number, record: any) => sum + record.Money_Out, 0);
+  this.sumMoneyOutPrev3Month = filteredPrev3MonthData.reduce((sum: number, record: any) => sum + record.Money_Out, 0);
+
+  console.log("Total Money Spent (Previous Month):", this.sumMoneyOutPrev1Month);
+  console.log("Total Money Spent (Two Months Ago):", this.sumMoneyOutPrev2Month);
+  console.log("Total Money Spent (Three Months Ago):", this.sumMoneyOutPrev3Month);
   
-    // Step 4: Calculate the sum of Money_Out for the filtered records
-    this.sumMoneyOut = this.filteredData.reduce((sum: number, record: any) => sum + record.Money_Out, 0);
-    console.log(this.sumMoneyOut);
-  
-    // Step 5: Log the filtered data
-    console.log(this.filteredData);
-  }
+  // Step 7: Calculate the total money spent for the current and previous three months
+  this.sumMoneyOut = this.sumMoneyOutCurrentMonth + this.sumMoneyOutPrev1Month + this.sumMoneyOutPrev2Month + this.sumMoneyOutPrev3Month;
+  console.log("Total Money Spent (Current + Previous 3 Months):", this.sumMoneyOut);
+}
 
   createChart() {
     const canvas: HTMLCanvasElement = document.getElementById('myChart') as HTMLCanvasElement;
