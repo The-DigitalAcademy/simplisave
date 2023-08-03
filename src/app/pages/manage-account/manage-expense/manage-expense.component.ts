b7a8a5ebd79ff8d4a06002361eecd245f60101f8
lebohang mokoena <lebodigitalacademy@gmail.com>
2023-08-03 16:48:02 +0200

    updated manage page functionality

--- src/app/pages/manage-account/manage-expense/manage-expense.component.ts
+++ src/app/pages/manage-account/manage-expense/manage-expense.component.ts
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/services/account.service';
+import { ManageModalComponent } from './manage-modal/manage-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { GoalModalComponent } from './goal-modal/goal-modal.component';
 
 @Component({
   selector: 'app-manage-expense',
@@ -9,13 +12,39 @@ import { AccountService } from 'src/app/services/account.service';
 })
 export class ManageExpenseComponent implements OnInit {
   Transaction_Type: any[] = [];
+  Goal_Savings: any[] = [];
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private dialog: MatDialog) { }
 
   ngOnInit() {
+   this.loadData();
+   this.goalSavings();
+  }
+
+  // 
+  loadData() {
     this.accountService.getTypes().subscribe((account) => {
       this.Transaction_Type = account;
-      // console.log(this.Transaction_Type);
+
+    });
+  }
+
+  goalSavings() {
+    this.accountService.getGoalSavings().subscribe((Amount_Set) =>{
+      this.Goal_Savings = Amount_Set;
+    });
+  }
+
+  // triggers onclick edit icon
+  openExpenseModal(): void {
+    const dialogRef = this.dialog.open(ManageModalComponent, {
+      width: '450px' // Set the desired width of the modal
+    });
+  }
+
+  openGoalModal(): void {
+    const dialogRef = this.dialog.open(GoalModalComponent, {
+      width: '450px' // Set the desired width of the modal
     });
   }
 
