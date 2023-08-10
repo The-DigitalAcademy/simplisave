17e29a8e1bda8c0fa2840678a7b484ed2d2f9597
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-08-10 15:38:16 +0200

    updated table for the transaction page

--- src/app/pages/Transactions/transactions/transaction-details/transaction-details.component.ts
+++ src/app/pages/Transactions/transactions/transaction-details/transaction-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'; // Import jQuery library
 
 @Component({
   selector: 'app-transaction-details',
   templateUrl: './transaction-details.component.html',
   styleUrls: ['./transaction-details.component.css']
 })
-export class TransactionDetailsComponent {
+export class TransactionDetailsComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Toggle filter dropdown visibility on mobile devices
+    $('#filterToggle').on('click', function() {
+      $('#filterDropdown').toggle();
+    });
+  }
 
 }
