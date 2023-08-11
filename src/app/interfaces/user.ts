9927d27bd1448c1d8e947942c742cd4a6c9ec137
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-08-11 15:50:14 +0200

    register

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,11 +1,9 @@
 export interface User { // interface for all data - Thilivhali Ravhutulu 07/08/2023
     name: string;
     email: string;
-    phone: string;
+    cellphoneNumber: string;
     password: string;
     confirmPassword: string;
-    accountNo: string;
-    pin: string;
     idNo: string;
     pinNo: string;
     confirmPinNo: string;
