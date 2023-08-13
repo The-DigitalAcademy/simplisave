47caeb093fdf80a361972a01ae6fdca0e0d4a1c9
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-08-14 00:32:51 +0200

    updated transactions functionality

--- src/app/pages/Transactions/transactions/balance-summary/balance-summary.component.ts
+++ src/app/pages/Transactions/transactions/balance-summary/balance-summary.component.ts
@@ -8,9 +8,13 @@ import { TransactionsService } from 'src/app/services/transactions.service';
   styleUrls: ['./balance-summary.component.css']
 })
 export class BalanceSummaryComponent implements OnInit{
-  currentBalance!: number;
-  totalIncome!: number;
-  totalExpenses!: number;
+  transactionsList: any[] = []; // Store your transactions here
+  totalIncome: number = 0; // Total income for the specific month
+  totalExpenses: number = 0; // Total expenses (money out) for the specific month
+  currentBalance: number = 0; // Current balance calculated as totalIncome - totalExpenses
+
+ 
+
 
   constructor(private http: HttpClient, private transactionService: TransactionsService) { }
 
@@ -18,14 +22,60 @@ export class BalanceSummaryComponent implements OnInit{
     this.fetchDataFromAPI();
   }
 
-  fetchDataFromAPI(): void {
-    this.transactionService.getTransactions().subscribe(
-      res => {
-        
-      },
-      error => {
-        console.error('Error fetching data:', error);
-      }
-    );
+  fetchDataFromAPI() {
+    this.transactionService.getTransactionsList()
+      .subscribe(
+        res => {
+          this.transactionsList = res;
+          this.calculateTotalsForMonth('08'); // Call the method with the desired month
+        },
+        error => {
+          console.error('Error fetching data:', error);
+        }
+      );
   }
+
+  calculateTotalsForMonth(month: string) {
+    const filteredTransactions = this.transactionsList.filter(transaction =>
+      transaction.transactionDate.includes(`-${month}-`)
+    );
+
+    const totalIncome = filteredTransactions.reduce((total, transaction) =>
+      total + transaction.moneyIn, 0);
+
+    const totalExpenses = filteredTransactions.reduce((total, transaction) =>
+      total + transaction.moneyOut, 0);
+
+    this.totalIncome = totalIncome;
+    this.totalExpenses = totalExpenses;
+    this.calculateCurrentBalance();
+  }
+
+  calculateCurrentBalance() {
+    this.currentBalance = this.totalIncome - this.totalExpenses;
+  }
+
+
+
+  /* calculateTotalIncome(): number {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+  
+    let totalIncome = 0;
+  
+    for (const details of this.transactionsList) {
+      const transactionDate = new Date(details.transactionDate);
+      const transactionYear = transactionDate.getFullYear();
+      const transactionMonth = transactionDate.getMonth();
+  
+      if (transactionYear === currentYear && transactionMonth === currentMonth) {
+        totalIncome += details.moneyIn;
+      }
+    }
+  
+    return totalIncome;
+  }*/
+
+  
 }
