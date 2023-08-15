a4b0e259e85b7ad01ea5cdfcb1da64ee16f13085
Mohammed Badat <moebadat16@gmail.com>
2023-08-15 16:30:23 +0200

    Profile dropdown

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -25,7 +25,6 @@ export class AccountService {
         const headers = new HttpHeaders({
           Authorization: `Bearer ${token}`
         });
-
         // Make the authenticated API request using HttpClient
         return this.http.get(`${environment.backendUrl}/auth/9`, { headers });
       })
