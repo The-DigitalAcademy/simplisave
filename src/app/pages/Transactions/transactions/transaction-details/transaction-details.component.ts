fbcd38fc79944288c226ffb6dd3c9bb2ec4b3523
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-08-16 16:23:22 +0200

    working on the datepicker for filtering transactions

--- src/app/pages/Transactions/transactions/transaction-details/transaction-details.component.ts
+++ src/app/pages/Transactions/transactions/transaction-details/transaction-details.component.ts
@@ -20,16 +20,39 @@ export class TransactionDetailsComponent implements OnInit{
   transactionsList: Transaction[] = [];
   groupedTransactions: any = {}; // Grouped transactions
   sortedDateKeys: string[] = []; // Declare sortedDateKeys property
+  // transactionsList: any[] = []; // Store your transactions here
+  totalIncome: number = 0; // Total income for the specific month
+  totalExpenses: number = 0; // Total expenses (money out) for the specific month
+  availableBalance: number = 0; // Current balance calculated as totalIncome - totalExpenses
+  currentBalance: any;
+  searchText: any;
+  searchForm: FormGroup = new FormGroup({});
+  filteredTransactions: Transaction[] = [];
+  filteredGroupedTransactions: any = {};
+  selectedDate: Date | null = null;
 
-  constructor(private transactionService: TransactionsService, private http: HttpClient) { }
-
-  ngOnInit(): void {
+  constructor(private transactionService: TransactionsService, private http: HttpClient, private formBuilder: FormBuilder)
+  {
+    this.searchForm = this.formBuilder.group({
+      transactionDate: [null],
+      description: [null],
+      amount:[null]
+    });
+ 
+   
     this.fetchDataFromAPI();
 
     // Toggle filter dropdown visibility on mobile devices
     $('#filterToggle').on('click', function() {
       $('#filterDropdown').toggle();
     });
+  }
+
+  ngOnInit(): void {
+    // this.fetchDataFromAPI();
+    // this.fetchDataFromAPI();
+    // this.getCurrentBalance();
+   
   }
 
   fetchDataFromAPI() {
@@ -48,19 +71,15 @@ export class TransactionDetailsComponent implements OnInit{
 
   groupTransactions() {
                                            
-                                           
     this.groupedTransactions = {};
 
-    // Iterate through each transaction in the transactionsList
     // Iterate through each transaction in the transactionsList
     for (const details of this.transactionsList) {
       // Extract the date from the transactionDate in 'YYYY-MM-DD' format
       const date = details.transactionDate.slice(0, 10);
 
       // Check if there is no existing array for this date in groupedTransactions
-      // Check if there is no existing array for this date in groupedTransactions
       if (!this.groupedTransactions[date]) {
-        // If not, create an empty array for this date in groupedTransactions
         // If not, create an empty array for this date in groupedTransactions
         this.groupedTransactions[date] = [];
       }
@@ -79,6 +98,77 @@ export class TransactionDetailsComponent implements OnInit{
 
   
 
+  // getCurrentBalance(){
+  //   this.transactionService.getCurrentBalance()
+  //     .subscribe(
+  //       res => {
+  //         this.currentBalance = res;
+  //         console.log(this.currentBalance);
+  //         this.availableBalance=this.currentBalance[0].Balance
+  //         console.log(this.availableBalance)
+  //       })
+  // }
+
+
+ 
+  applyFilter(event: MatDatepickerInputEvent<Date>) {
+    if (event.value) {
+      // When a date is selected from the date picker
+      this.selectedDate = event.value;
+  
+      // Initialize an empty object to store filtered grouped transactions
+      this.filteredGroupedTransactions = {};
+  
+      // Iterate through each date in the grouped transactions
+      for (const date in this.groupedTransactions) {
+        // Filter details for the current date based on the selected date
+        const filteredDetails = this.groupedTransactions[date].filter((details: any) => {
+          const transactionDate = new Date(details.transactionDate);
+  
+          // Check if the selectedDate matches the transaction's date
+          if (this.selectedDate) {
+            return (
+              transactionDate.getFullYear() === this.selectedDate.getFullYear() &&
+              transactionDate.getMonth() === this.selectedDate.getMonth() &&
+              transactionDate.getDate() === this.selectedDate.getDate()
+            );
+          }
+  
+          return false; // Return false if selectedDate is null
+        });
+  
+        // Store the filtered details for the current date
+        if (filteredDetails.length > 0) {
+          this.filteredGroupedTransactions[date] = filteredDetails;
+        }
+      }
+    } else {
+      // When no date is selected, reset the selectedDate and show all transactions
+      this.selectedDate = null;
+      this.filteredGroupedTransactions = this.groupedTransactions;
+    }
+  }
+  
+ 
+ 
+  isSelectedDate(transactionDate: string): boolean {
+    // Check if a date is selected
+    if (this.selectedDate) {
+      // Convert the selected date to a string in 'YYYY-MM-DD' format
+      const selectedDateStr = this.selectedDate.toISOString().substring(0, 10);
+  
+      // Check if the transaction date starts with the selected date string
+      return transactionDate.startsWith(selectedDateStr);
+    }
+  
+    return false; // Return false if no date is selected
+  }
+  
+ 
+ 
+ 
+ 
+ 
   // applyDateFilter(): void {
   //   if (this.selectedDate) {
   //     this.filteredTransactions = this.transactions.filter((transaction: { date: string; }) =>
