132e245a6f23f70c998360e69e3574500c6d96b4
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-08-18 10:51:01 +0200

    Modified the login alert

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -46,8 +46,8 @@ export class AuthService {
   successAlert(){
     Swal.fire({
       icon: 'success',
-      title: 'Login Successful!!!',
-      text: 'Welcome to simpliSave',
+      title: 'Login Successful',
+      text: 'Welcome to SimpliSave',
       iconColor: '#AF144B',
       confirmButtonColor: '#AF144B'
     });
@@ -57,7 +57,7 @@ export class AuthService {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
-      text: 'User not found or incorrect credentials. Please try again!!!',
+      text: 'User not found or incorrect credentials. Please try again',
       iconColor: '#AF144B',
       confirmButtonColor: '#AF144B'
     });
