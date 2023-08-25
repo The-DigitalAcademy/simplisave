4285a9901deef72d41e19f1563a5a1458118eca0
lebohang mokoena <lebodigitalacademy@gmail.com>
2023-08-25 08:35:06 +0200

    updated main branch

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -6,104 +6,104 @@ import { environment } from '../../environments/environment'; // Import environm
 import { AuthService } from './auth-service.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AccountService {
+    private refreshSubject = new Subject<void>();
 
-  private refreshSubject = new Subject<void>();
+    refreshObservable = this.refreshSubject.asObservable();
 
-  refreshObservable = this.refreshSubject.asObservable();
+    private url = `${environment.apiUrl}/Goal_Savings`;
 
- 
-  private url = `${environment.apiUrl}/Goal_Savings`;
+    constructor(
+        private http: HttpClient,
+        private authService: AuthService
+    ) {}
 
-  constructor(private http: HttpClient, private authService:AuthService) { }
+    // getAccountData() {
+    //   console.log(this.authService.getToken())
+    //   return this.authService.getToken().pipe(
+    //     switchMap(token => {
+    //       const headers = new HttpHeaders({
+    //         Authorization: `Bearer ${token}`
+    //       });
+    //       // Make the authenticated API request using HttpClient
+    //       return this.http.get(`${environment.backendUrl}/accounts/account-balance`, { headers });
+    //     })
+    //   );
 
-  // getAccountData() {
-  //   console.log(this.authService.getToken())
-  //   return this.authService.getToken().pipe(
-  //     switchMap(token => {
-  //       const headers = new HttpHeaders({
-  //         Authorization: `Bearer ${token}`
-  //       });
-  //       // Make the authenticated API request using HttpClient
-  //       return this.http.get(`${environment.backendUrl}/accounts/account-balance`, { headers });
-  //     })
-  //   );
-    
-  getAccountData(){
-    return this.http.get(`${environment.apiUrl}/Account`);
-  }
+    getAccountData() {
+        return this.http.get(`${environment.apiUrl}/Account`);
+    }
 
-  getTransactions(){
-    return this.http.get(`${environment.apiUrl}/Transaction`);
-  }
+    getTransactions() {
+        return this.http.get(`${environment.apiUrl}/Transaction`);
+    }
 
-  getTransactions2(){
-    return this.http.get(`${environment.apiUrl}/Transaction`);
-  }
+    getTransactions2() {
+        return this.http.get(`${environment.apiUrl}/Transaction`);
+    }
 
-  getTypes(): Observable<any[]> {
-    return this.http.get<any[]>(`${environment.apiUrl}/Budget`);
-  }
+    getTypes(): Observable<any[]> {
+        return this.http.get<any[]>(`${environment.apiUrl}/Budget`);
+    }
 
-  getSimplisaveData(){
-    return this.http.get(`${environment.apiUrl}/Simpil_Savings_Account`);
-  }
+    getSimplisaveData() {
+        return this.http.get(`${environment.apiUrl}/Simpil_Savings_Account`);
+    }
 
-  createType(body: any): Observable<any>{
-    return this.http.post<any>(`${environment.apiUrl}/Budget`, body);
-  }
+    createType(body: any): Observable<any> {
+        return this.http.post<any>(`${environment.apiUrl}/Budget`, body);
+    }
 
-  getGoalSavings(): Observable<any[]> {
-    return this.http.get<[any]>(`${environment.apiUrl}/Goal_Savings`);
-  }
+    getGoalSavings(): Observable<any[]> {
+        return this.http.get<[any]>(`${environment.apiUrl}/Goal_Savings`);
+    }
 
-  updateGoalSavings(data: any, id: any): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/Budget/${id}`, data);
-  }
+    updateGoalSavings(data: any, id: any): Observable<any> {
+        return this.http.put(`${environment.apiUrl}/Budget/${id}`, data);
+    }
 
-  updateGoalSaving(data: any, id: any): Observable<any> {
-    return this.http.put(`${this.url}/${id}`, data);
-  }
+    updateGoalSaving(data: any, id: any): Observable<any> {
+        return this.http.put(`${this.url}/${id}`, data);
+    }
 
-  getUser(id:any){
-    return this.http.get(`${environment.apiUrl}/User/${id}`);
-  }
+    getUser(id: any) {
+        return this.http.get(`${environment.apiUrl}/User/${id}`);
+    }
 
-  updateUser(id:any,data:any){
-    return this.http.put(`${environment.apiUrl}/User/${id}`,data);
-  }
-  getOneTransaction(id: any): Observable<void> {
-    return this.http.get<void>(`${environment.apiUrl}/Budget/${id}`);
-  }
+    updateUser(id: any, data: any) {
+        return this.http.put(`${environment.apiUrl}/User/${id}`, data);
+    }
+    getOneTransaction(id: any): Observable<void> {
+        return this.http.get<void>(`${environment.apiUrl}/Budget/${id}`);
+    }
 
+    //Refreshes the page after a successful update
+    // Lebohang Mokoena
+    // 2023/08/07
+    deleteTransaction(id: any): Observable<void> {
+        return this.http.delete<void>(`${environment.apiUrl}/Budget/${id}`);
+    }
 
-  //Refreshes the page after a successful update
-  // Lebohang Mokoena
-  // 2023/08/07
-  deleteTransaction(id: any): Observable<void> {
-    return this.http.delete<void>(`${environment.apiUrl}/Budget/${id}`);
-  }
+    transferToSavings(data: any) {
+        console.log(this.authService.getToken());
+        return this.authService.getToken().pipe(
+            switchMap(token => {
+                const headers = new HttpHeaders({
+                    Authorization: `Bearer ${token}`,
+                });
+                // Make the authenticated API request using HttpClient
+                return this.http.post(
+                    `${environment.backendUrl}/accounts/transfer/8`,
+                    data,
+                    { headers }
+                );
+            })
+        );
+    }
 
-
-
-  transferToSavings(data:any) {
-    console.log(this.authService.getToken())
-    return this.authService.getToken().pipe(
-      switchMap(token => {
-        const headers = new HttpHeaders({
-          Authorization: `Bearer ${token}`
-        });
-        // Make the authenticated API request using HttpClient
-        return this.http.post(`${environment.backendUrl}/accounts/transfer/8`, data, { headers });
-      })
-    );
-  }
-
-
-
-  triggerRefresh() {
-    this.refreshSubject.next();
-  }
+    triggerRefresh() {
+        this.refreshSubject.next();
+    }
 }
