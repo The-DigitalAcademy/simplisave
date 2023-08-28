dcb71d39cc3f78310bf5c6ca137d2e6cb04e0da6
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-08-28 10:29:20 +0200

    updated endpoints for JSON authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
 export const environment = {
     production: true,
-    REG_URL: 'https://springsimplisave-production.up.railway.app/api/auth/register',
+    // REG_URL: 'https://simplisave.software/api/v1/student/registration',
+    REG_URL: 'http://localhost:3000/register',
     LOGIN_URL: 'http://localhost:8080/api/users/login',
     apiUrl: 'http://localhost:3000',
     backendUrl: 'https://simplisave.software/api/v1',
-    FORGOT_URL:'https://springsimplisave-production.up.railway.app/api/auth/forgot-password',
-    RESET_URL:'https://springsimplisave-production.up.railway.app/api/auth/reset-passsword'
+    // FORGOT_URL:'https://springsimplisave-production.up.railway.app/api/auth/forgot-password',
+    FORGOT_URL: 'http://localhost:3000/forgot',
+    // RESET_URL:'https://springsimplisave-production.up.railway.app/api/auth/reset-passsword'
+    RESET_URL: 'http://localhost:3000/reset'
 };
