bfa8a97494988c01b3832cc8cb8780ef0ff5916c
Mukosi <mukosibudeli@gmail.com>
2023-08-30 16:14:45 +0200

    worked on admin , created  admin navbar

--- src/app/components/admin/admin/admin.component.ts
+++ src/app/components/admin/admin/admin.component.ts
@@ -11,7 +11,7 @@ import { Student } from 'src/app/interfaces/students';
 })
 export class AdminComponent implements OnInit {
 
-  students: any;
+  students: Student[] = [];
   studs: any;
 
   constructor(
@@ -35,14 +35,18 @@ export class AdminComponent implements OnInit {
   }
 
   getList() {
-
-    this.studentsList.getStudents()
-      .subscribe(res => {
-        this.studs = res;
+    this.studentsList.getStudents().subscribe({
+      next: res => { // Use the correct type for the response data
+        console.log(res); // Log the API response to the console
+        this.studs = res; // Store fetched students in the students array
         this.students=this.studs.data;
-        console.log(this.studs);
-        
-        });
+        // console.log( this.studs); 
+
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
   removeStudent(id: number) {
