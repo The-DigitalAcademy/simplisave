aa584bea8850af8477a663fa98a6c0eebd0987b4
Mukosi <mukosibudeli@gmail.com>
2023-08-30 11:10:14 +0200

    Worked on admin

--- src/app/components/admin/admin/admin.component.ts
+++ src/app/components/admin/admin/admin.component.ts
@@ -1,12 +1,8 @@
-import { DialogRef } from '@angular/cdk/dialog';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddStudentsComponent } from '../add-students/add-students.component';
 import { StudentsService } from 'src/app/services/students.service';
-import {AfterViewInit, ViewChild} from '@angular/core';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
+import { Student } from 'src/app/interfaces/students';
 
 @Component({
   selector: 'app-admin',
@@ -15,112 +11,63 @@ import { MatSort } from '@angular/material/sort';
 })
 export class AdminComponent implements OnInit {
 
-  displayColumn: string[]=[
-    'userId',
-    'firstName',
-    'lastName',
-    'username',
-    'email',
-    'cellphoneNumber',
-    'action'
-    
-  ];
-
-  displayedColumns: string[] = [  
-  'userId',
-  'firstName',
-  'lastName',
-  'username',
-  'email',
-  'cellphoneNumber',
-  'action'
-
-];
-
-  dataSource!:  MatTableDataSource<any>;
-
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
-
-  @ViewChild(MatSort)
-  sort!: MatSort;
+  students: any;
+  studs: any;
 
   constructor(
-    private addDialog:MatDialog,
-    private studentsList:StudentsService
-    
-    ){}
-    
-    ngOnInit(){
+    private addDialog: MatDialog,
+    private studentsList: StudentsService
+  ) { }
 
-      this.getList()
-    }
+  ngOnInit() {
+    this.getList();
+  }
 
-
-
-  openAddStudents(){
-    const dialogRef = this.addDialog.open(AddStudentsComponent)
+  openAddStudents() {
+    const dialogRef = this.addDialog.open(AddStudentsComponent);
     dialogRef.afterClosed().subscribe({
-      next:(value)=>{
-        if(value ){
+      next: (value) => {
+        if (value) {
           this.getList();
         }
       }
-    })
+    });
   }
 
   getList() {
-    this.studentsList.getStudents().subscribe({
-      next: (res: any) => {
-        console.log(res); // Check if the data is being received
+
+    this.studentsList.getStudents()
+      .subscribe(res => {
+        this.studs = res;
+        this.students=this.studs.data;
+        console.log(this.studs);
         
+        });
+  }
+
+  removeStudent(id: number) {
+    this.studentsList.deleteStudent(id).subscribe({
+      next: (res) => {
+        alert('Student Deleted!');
+        this.getList();
       },
       error: (err) => {
         console.log(err);
       }
     });
   }
-  
 
-  // getList(){
-  //   this.studentsList.getStudents().subscribe({
-  //     next: (res: any)=>{  //if successful
-  //      this.dataSource = new MatTableDataSource(res);
-  //      this.dataSource.sort = this.sort;
-  //      this.dataSource.paginator = this.paginator
-         
-  //     },
-  //     error: (err) =>{ //else return an error
-  //       console.log(err)
-  //     }
-  //   })
-  // }
-removeStudent(id: number){
-  this.studentsList.deleteStudent(id).subscribe({
-    next: (res) =>{
-alert('Student Deleted!')
-this.getList();
-    },
-    error: (err) =>{
-      console.log(err);
-      
-    }
-    
-  })
-}
+  openEditStudents(data: any) {
+    const dialogRef = this.addDialog.open(AddStudentsComponent, {
+      data,
+    });
 
-openEditStudents(data: any){
-  const dialogRef = this.addDialog.open(AddStudentsComponent, {
-    data,
-  })
-
-  dialogRef.afterClosed().subscribe({
-    next:(value)=>{
-      if(value ){
-        this.getList();
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getList();
+        }
       }
-    }
-  })
-}
-  
+    });
+  }
 }
