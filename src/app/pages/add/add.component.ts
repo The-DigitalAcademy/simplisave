071cd106c0cab7505bd9acbe99739fd40a74632e
Thilivhali Ravhutulu <ravhutuluportia@gmail.com>
2023-09-18 19:18:18 +0200

    Added formGroup and formBuilder

--- src/app/pages/add/add.component.ts
+++ src/app/pages/add/add.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+
+  addTransaction!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit() {
+    // Define form fields for login and validation requirements
+    this.addTransaction = this.formBuilder.group({
+      type: [
+        '',
+        [
+          Validators.required
+        ],
+      ],
+      description: [
+        '',
+        [
+          Validators.required
+        ],
+      ],
+      amount: [
+        '',
+        [
+          Validators.required
+        ],
+      ],
+    });
+  }
+
+  
 
 }
